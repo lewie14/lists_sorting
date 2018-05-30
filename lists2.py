526cd67bd333f4bc9d8576b665157d26b4dc91c8
lewie14 <lewie@netvigator.com>
2018-05-30 12:45:14 +0800

    First commit - List examples 2

---
+++ lists2.py
@@ -0,0 +1,15 @@
+list1 = range(2, 20, 2)
+
+#Find length of list1
+list1_len = len(list1)
+print(list1_len)
+
+#Change the list range so it skips 3 instead of 2 numbers
+
+list1 = range(2, 20, 3)
+
+#Find length of list1
+list1_len = len(list1)
+print(list1_len)
+
+
