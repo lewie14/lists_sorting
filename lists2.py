beb084aeab87b24a62bff40d99a00f107d2a2324
lewie14 <lewie@netvigator.com>
2018-05-30 12:51:04 +0800

    first indexes

--- lists2.py
+++ lists2.py
@@ -11,5 +11,13 @@ list1 = range(2, 20, 3)
 #Find length of list1
 list1_len = len(list1)
 print(list1_len)
+print()
+
+#------------------------------------------------------
+
+#indexes
+employees = ['Michael', 'Dwight', 'Jim', 'Pam', 'Ryan', 'Andy', 'Robert']
+index4 = employees[4]
+print(len(employees))
 
 
