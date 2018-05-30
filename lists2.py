be5218c258c87fe333466cda6ae15f4e9c652c07
lewie14 <lewie@netvigator.com>
2018-05-30 13:42:53 +0800

    Sorting lists

--- lists2.py
+++ lists2.py
@@ -50,7 +50,47 @@ suitcase = ['shirt', 'shirt', 'pants', 'pants', 'pajamas', 'books']
 start = suitcase[:3]
 #Select last 2 items
 end = suitcase[-2:]
+print()
 
 #------------------------------------------------------
 
 #Counting elements in a list
+
+#Mrs. Wilson's class is voting for class president. She has saved each student's vote into the list votes.
+#Use count to determine how many students voted for 'Jake'. Save your answer as jake_votes.
+votes = ['Jake', 'Jake', 'Laurie', 'Laurie', 'Laurie', 'Jake', 'Jake', 'Jake', 'Laurie', 'Cassie', 'Cassie', 'Jake', 'Jake', 'Cassie', 'Laurie', 'Cassie', 'Jake', 'Jake', 'Cassie', 'Laurie']
+jake_votes = votes.count('Jake')
+print(jake_votes)
+print()
+
+#------------------------------------------------------
+
+#Sorting elements in a list
+### Exercise 1 & 2 ###
+addresses = ['221 B Baker St.', '42 Wallaby Way', '12 Grimmauld Place', '742 Evergreen Terrace', '1600 Pennsylvania Ave', '10 Downing St.']
+
+# Sort addresses here:
+addresses.sort()
+print(addresses)
+
+### Exercise 3 ###
+names = ['Ron', 'Hermione', 'Harry', 'Albus', 'Sirius']
+names.sort()
+
+### Exercise 4 ###
+cities = ['London', 'Paris', 'Rome', 'Los Angeles', 'New York']
+#The print doesnt work because the sort does not return a sorted list to a variable
+sorted_cities = cities.sort()
+print(sorted_cities)
+print()
+
+games = ['Portal', 'Minecraft', 'Pacman', 'Tetris', 'The Sims', 'Pokemon']
+#Use sorted to order games and create a new list called games_sorted
+games_sorted = sorted(games)
+#Use print to inspect games and games_sorted
+print(games)
+print(games_sorted)
+
+#------------------------------
+#All together
+
